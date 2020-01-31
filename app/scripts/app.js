b0b9d2209c142b068b621901bc209c055d56286c
Julian <juferus12@gmail.com>
2020-01-31 15:02:21 -0500

    Authentication facebook with cognito

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,6 +3,11 @@
 const myApp = angular.module('testAppApp', ['ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider
+      .when('/main', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
       .when('/', {
         templateUrl: 'views/login.html',
         controller: 'AuthenticationCtrl',
@@ -15,30 +20,21 @@ const myApp = angular.module('testAppApp', ['ngRoute'])
 
 window.fbAsyncInit = function() {
     FB.init({
-      appId      : '829762180830799',
-      cookie     : true,
+      appId      : '******************',
+      cookie     : false,
       xfbml      : true,
       version    : 'v2.3'
-    });
-
-
+    })
     FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  };
+      statusChangeCallback(response)
+    })
+  }
 
   
-  (function(d, s, id) {                      // Load the SDK asynchronously
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
-app.service('cognitoService', function () {
-  AWS.config.region = 'us-east-1'
-  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: ''
-  })
-})
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0]
+    if (d.getElementById(id)) return
+    js = d.createElement(s); js.id = id
+    js.src = "https://connect.facebook.net/en_US/sdk.js"
+    fjs.parentNode.insertBefore(js, fjs)
+  }(document, 'script', 'facebook-jssdk'))
--- app/scripts/controllers/facebook.js
+++ app/scripts/controllers/facebook.js
@@ -5,12 +5,33 @@ myApp.controller('AuthenticationCtrl', ['$scope', function($scope) {
   $scope.FBLogin = function () {
     FB.login(function(response) {
       if (response.authResponse) {
-        console.log('Welcome')
-          console.log(response)
+        $scope.tokenFacebook = response.authResponse.accessToken
+        AWS.config.region = 'us-east-1';
+            AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                AccountId: '*************',
+                RoleARN: 'arn:aws:iam::*************:role/Cognito_TestFacebookAuth_Role',
+                IdentityPoolId: 'us-east-1:********-****-****-****-************',
+                Logins: {
+                    'graph.facebook.com': response.authResponse.accessToken
+                }
+            })
+            AWS.config.credentials.get(function(){
+                let accessKeyId = AWS.config.credentials.accessKeyId;
+                let secretAccessKey = AWS.config.credentials.secretAccessKey;
+                let sessionToken = AWS.config.credentials.sessionToken;
+                let credentials = AWS.config.credentials
+								console.log("CREDENCIALES DE AWS: ",credentials)
+                return credentials
+            }) 
+        console.log('Welcome to the application!')
         FB.api('/me', function(response) {
           console.log('Successful login for: ' + response.name)
           console.log(response)
           document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!';
+          let accessToken = FB.getAuthResponse()
+          console.log(accessToken)
+          window.location = "http://localhost:9000/#!/main"
+          // console.log(sessionStorage.token)
         })
       } else {
         console.log('User cancelled login')
